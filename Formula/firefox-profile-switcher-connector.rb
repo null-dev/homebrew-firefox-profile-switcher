d6c666cabf111108321f13288bf2e0f0e00197ab
Andy Bao <contact@andybao.me>
2021-06-19 22:32:48 -0400

    Update to v0.0.7

--- Formula/firefox-profile-switcher-connector.rb
+++ Formula/firefox-profile-switcher-connector.rb
@@ -2,9 +2,9 @@ class FirefoxProfileSwitcherConnector < Formula
 
     desc "The native component of the Profile Switcher for Firefox extension."
     homepage "https://github.com/null-dev/firefox-profile-switcher-connector"
-    url "https://github.com/null-dev/firefox-profile-switcher-connector/archive/refs/tags/v0.0.6.tar.gz"
-    sha256 "40f0429f2aeebd128072a75674e132477ef1f16ce03d77704fa55395624a62fc"
-    version "0.0.6"
+    url "https://github.com/null-dev/firefox-profile-switcher-connector/archive/refs/tags/v0.0.7.tar.gz"
+    sha256 "1fe2075e2a5bd42455b07771f3e6a08cdee6945e859a304ce715899d505555d6"
+    version "0.0.7"
     depends_on "rust" => :build
 
     @@manifest_name = "ax.nd.profile_switcher_ff.json"
